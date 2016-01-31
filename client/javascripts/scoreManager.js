2b468a47e308578ccb4f3c3204a0da7c0b231060
Cory Williams <cjwillia@andrew.cmu.edu>
2016-01-31 12:59:31 -0500

    consistency ayyyyyy

--- client/javascripts/scoreManager.js
+++ client/javascripts/scoreManager.js
@@ -1,16 +1,3 @@
-var team1Data = [
-    {name: "p2", score: 42},
-    {name: "p4", score: 23},
-    {name: "p3", score: 5},
-    {name: "p1", score: 78}
-];
-
-var team2Data = [
-    {name: "p5", score: 14},
-    {name: "p6", score: 31},
-    {name: "p7", score: 28}
-];
-
 var body = d3.select("body");
 
 function sortList(listId) {
@@ -48,25 +35,3 @@ function scoreCompare(d1, d2) {
 function formatPlayerInfo (d) {
     return "<span class='name'>"+d.name+"</span>" + "<span class='score'>"+d.score+"</span>";
 }
-
-insertData("#team1list", team1Data);
-insertData("#team2list", team2Data);
-
-sortList("#team1list");
-sortList("#team2list");
-
-team2Data = [
-    {name: "p5", score: 14},
-    {name: "p6", score: 31},
-    {name: "p7", score: 0},
-    {name: "p8", score: 600}
-];
-
-updateData("#team2list", team2Data);
-
-team1Data = [
-    {name: "p1", score: 55},
-    {name: "p9", score: 5555}
-];
-
-updateData("#team1list", team1Data);
